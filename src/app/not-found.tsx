81a26ed5e573aaf43db2c5e63709fdede66f16ed
Karthik150502 <karthikrdycool@gmail.com>
2025-09-17 23:48:01 +0530

    init

---
+++ src/app/not-found.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+import React from 'react'
+
+export default function NotFoundPage() {
+    return (
+        <div className='w-full h-full flex flex-col items-start justify-start gap-2'>
+            <p>Sorry the page you are looking for does not exist, click here to go back to <span className='font-bold underline'><Link href={"/"}>
+                Home
+            </Link></span></p>
+        </div>
+    )
+}
